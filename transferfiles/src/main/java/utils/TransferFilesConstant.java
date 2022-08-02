99d7ada9f45021d2b5721e80f57d30569492ac2c
Dwarakeshwaran Mageshkumar <30549378+Dwarakeshwaran@users.noreply.github.com>
2022-08-02 23:50:02 +0530

    Transfer File Handler and Constants Added

---
+++ transferfiles/src/main/java/utils/TransferFilesConstant.java
@@ -0,0 +1,14 @@
+package utils;
+
+public class TransferFilesConstant {
+	
+	private TransferFilesConstant() {
+	}
+	
+	public static final String S3_PROTOCOL = "S3";
+	public static final String SFTP_PROTOCOL = "SFTP";
+	public static final String FTPS_PROTOCOL = "FTPS";
+	
+	public static final String TEMP_FOLDER_PATH = "tmp/";
+
+}
