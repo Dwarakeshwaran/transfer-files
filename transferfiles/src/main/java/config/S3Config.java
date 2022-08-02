e67fc8dfbe4ea60c5d875d5d8275a875491a9fc9
Dwarakeshwaran Mageshkumar <30549378+Dwarakeshwaran@users.noreply.github.com>
2022-08-02 23:49:23 +0530

    S3 Config and Operations File Added

---
+++ transferfiles/src/main/java/config/S3Config.java
@@ -0,0 +1,16 @@
+package config;
+
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+
+public class S3Config {
+
+	public AmazonS3 getS3Config() {
+
+		return AmazonS3ClientBuilder.standard().withCredentials(new DefaultAWSCredentialsProviderChain())
+				.withRegion(Regions.US_EAST_1).build();
+	}
+
+}
